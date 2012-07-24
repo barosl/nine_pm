613d9c95785469b495912f968a765ad785de46be
Barosl LEE <vcs@barosl.com>
2012-07-24 20:57:10 +0900

    Fix a bug that accesses wrong url

--- static/js/user-stay.js
+++ static/js/user-stay.js
@@ -1,5 +1,5 @@
 setInterval(function() {
 	var obj = document.createElement('script');
-	obj.src = '/user_stay/'+forum_id+'/';
+	obj.src = '/user/stay/'+forum_id+'/';
 	document.body.appendChild(obj);
 }, 1000*5);
