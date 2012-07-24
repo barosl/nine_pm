94d34440e37260e7d73ecf45ce0c951b85e7d98d
Barosl LEE <vcs@barosl.com>
2012-07-24 12:02:14 +0900

    Add some models

--- models.py
+++ models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Forum(models.Model):
+	name = models.CharField(max_length=100)
+
+	ex_rate = models.IntegerField()
+	img = models.ImageField(upload_to='forums')
+
+class Post(models.Model):
+	forum = models.ForeignKey(Forum)
+	user = models.ForeignKey(User)
+
+	name = models.CharField(max_length=100)
+	text = models.TextField()
+	ctime = models.DateTimeField()
+	mtime = models.DateTimeField()
+	points = models.IntegerField()
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post)
+	user = models.ForeignKey(User)
+
+	text = models.TextField()
+	ctime = models.DateTimeField()
+	mtime = models.DateTimeField()
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+
+	name = models.CharField(max_length=100)
+
+class UserPoints(models.Model):
+	user = models.ForeignKey(User)
+	forum = models.ForeignKey(Forum)
+	mtime = models.IntegerField()
+	points = models.IntegerField()
