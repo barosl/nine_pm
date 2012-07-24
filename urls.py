cea18aa041f0abdc7d6d1b51f9435110b2b2d83d
Barosl LEE <vcs@barosl.com>
2012-07-24 14:29:55 +0900

    Add login functionality

--- urls.py
+++ urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
+from views import *
+from django.contrib.auth.views import login, logout
 
-urlpatterns = patterns('nine_pm.views',
-	url(r'', 'index'),
+urlpatterns = patterns('',
+	url(r'^$', index),
+	url(r'^login/$', login, {'template_name': 'index.html'}),
+	url(r'^logout/$', logout, {'next_page': '/'}),
 )
--- views.py
+++ views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(req):
-	return render_to_response('index.html')
+	return render_to_response('index.html', RequestContext(req, {
+		'form': AuthenticationForm()
+	}))
